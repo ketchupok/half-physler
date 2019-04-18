8c2e9eaca4a646efce218a9f1fd2c3a2800c8bbb
ketchupok <ahah@gmx.net>
2019-04-18 16:21:10 +0200

    added resonator_visco_concat_pointers to opcode registers

--- src/opcode_registers.cpp
+++ src/opcode_registers.cpp
@@ -1,5 +1,6 @@
 #include "../resonators/cone_radiation_losses_vp_pointers_PC.cpp"
 #include "../resonators/cone_radiation_losses_vp_pointers_fixedM_Bela.cpp"
+#include "../resonators/resonator_visco_concat_pointers.cpp"
 
 
 
@@ -8,4 +9,5 @@
 void csnd::on_load(Csound *csound) {
   csnd::plugin<Cone_Radiation_Losses>(csound, "halfphysler", "aa", "akkkkkk", csnd::thread::ia);
   csnd::plugin<Cone_Radiation_Losses_fixedM_Bela>(csound, "halfphysler_bela", "aa", "akkkkkk", csnd::thread::ia);
+  csnd::plugin<Resonator_Visco_Concat_Pointers>(csound, "tube_resonator", "a", "aki[]i[]i[]i[]", csnd::thread::ia);
 }
